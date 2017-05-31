4f09da5e87b8a83ec962904e02bca7c51d277b27
Eliseo Marin Navarro <Nesias_M@hotmail.com>
2017-05-31 01:01:38 -0500

    pg bienvenida

---
+++ CURSO DE TALLER DE SOFWARE IV/BIENVENIDO.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BIENVENIDO.aspx.cs" Inherits="CURSO_DE_TALLER_DE_SOFWARE_IV.BIENVENIDO" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            PAGINA DE TALLER DE SOFTWARE<br />
+        </div>
+    </form>
+    <p>HOLA</p>
+</body>
+</html>
---
+++ CURSO DE TALLER DE SOFWARE IV/BIENVENIDO.aspx.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CURSO_DE_TALLER_DE_SOFWARE_IV {
+    
+    
+    public partial class BIENVENIDO {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
